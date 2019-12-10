045e46d3aaf4f4f137164d82fc256a02d14ee209
t3rmin41 <t33rmin41@gmail.com>
2019-12-10 09:51:06 +0200

    Logback added

--- src/main/java/com/simple/api/Main.java
+++ src/main/java/com/simple/api/Main.java
@@ -1,9 +1,14 @@
 package com.simple.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String args[]) {
-        System.out.println("Start REST API");
+        log.info("Start REST API");
     }
 
 }
